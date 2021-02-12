73588cbc2e76634172a169bce1275274fbf898e9
Leonardo Zavala <lezavala@globe.life>
2021-02-11 17:07:15 -0800

    Fix dynamic block

--- main.tf
+++ main.tf
@@ -227,7 +227,7 @@ resource "aws_codebuild_project" "default" {
       for_each = aws_s3_bucket.cache_bucket
       content {
         type  = "S3"
-        location = cache.value["bucket"]
+        location = cache.value["id"]
       }
   }
 
